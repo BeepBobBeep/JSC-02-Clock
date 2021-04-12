0a484159ee5b211a76c84fe92be7622684f1d94c
BeepBopBeep <qyburgstark@gmail.com>
2021-04-12 10:02:37 +0530

    Making second hand come over the other two Making second hand's COR at 20% - line 15,16 in index.js and 59-65 in style.css

--- index.js
+++ index.js
@@ -12,18 +12,19 @@ function moveSecondHand (){
             document.querySelector(".second-hand").classList.remove("no-effect");    
         }, 10);
     }
-    document.querySelector(".second-hand").style.transform = `rotate(${secDegree}deg)`;
+    // 20% to make second hand centre of rotation at 20% and 80% ratio
+    document.querySelector(".second-hand").style.transform = `translate(-20%,-50%) rotate(${secDegree}deg)`;
     
     var min = now.getMinutes();
-    var minDegree = (min * 6 + sec/6 - 90);
+    var minDegree = (min * 6 + sec/10 - 90);
     // Dealing with the glitch effect when sec is changing from 59 to 0
-    if(min == 0){
+    if(sec == 0){
         // Removing the transition effect
         document.querySelector(".minute-hand").classList.add("no-effect");
         setTimeout(function(){
             // Adding the transition effect
             document.querySelector(".minute-hand").classList.remove("no-effect");    
-        }, 10);
+        }, 0);
     }
     document.querySelector(".minute-hand").style.transform = `rotate(${minDegree}deg)`;
 
@@ -42,4 +43,4 @@ function moveSecondHand (){
 }
 
 // Updating hands every 1 second
-setInterval(moveSecondHand,1000);
+// setInterval(moveSecondHand,1000);
